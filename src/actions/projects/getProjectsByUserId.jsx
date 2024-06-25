c673b2e300a800c133c13051fccab0cbef38e228
Agustin Aused <agus.aused@gmail.com>
2024-06-25 00:08:30 -0300

    .

---
+++ src/actions/projects/getProjectsByUserId.jsx
@@ -0,0 +1,29 @@
+'use server';
+import { cookies } from "next/headers";
+
+export default async function getProjectsByUserId(userId) {
+    const cookieStore = cookies();
+    const token = cookieStore.get("access_token")?.value || "";
+    try {
+        const user_id = parseInt(userId);
+        const response = await fetch(`http://localhost:8081/projects/user/${user_id}`, {
+            method: "GET",
+            cache: "no-store",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+            },
+        });
+        // console.log("Full Response:", response);
+        const data = await response.json();
+        console.log("Parsed JSON Data:", data);
+        // console.log("Parsed JSON Data:", data);
+        return data;
+    } catch (error) {
+        console.error("Error:", error);
+        // You might want to handle the error here, depending on your use case
+        throw error;
+    }
+    
+};
+
